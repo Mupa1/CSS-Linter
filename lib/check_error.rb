4e12d83fa24dfe366d4de846070e15d16b43cf42
Mupa1 <mupasmail@gmail.com>
2020-06-07 20:30:41 +0200

    add methods for checking a missing ending semicolon

--- lib/check_error.rb
+++ lib/check_error.rb
@@ -2,5 +2,16 @@ require_relative 'errors.rb'
 
 class ErrorChecker
   include Errors
-  
+  attr_reader :file_data_arr
+
+  def initialize(file_data_arr)
+    @file_data_arr = file_data_arr
+  end
+
+  def display_error
+    file_data_arr.each_with_index do |file_data, index|
+      data_index = index + 1
+      missing_ending_semicolon?(file_data, data_index)
+    end
+  end
 end
--- lib/errors.rb
+++ lib/errors.rb
@@ -1,3 +1,11 @@
+require 'colorize'
+
 module Errors
-  
+  def missing_ending_semicolon?(file_data, data_index)
+    if file_data.match?(/(;.*)$/) || file_data.match?(/({|})/) || file_data.match?(/^\n$/)
+      true
+    else
+      puts 'Error: '.red + 'Missing ending semicolon ";" on' + " line:[:#{data_index}:]".yellow
+    end
+  end
 end
