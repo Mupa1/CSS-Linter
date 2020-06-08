f440a0dda36f30f061eda4fac53fc2a3b0f7207a
Mupa1 <mupasmail@gmail.com>
2020-06-08 16:46:29 +0200

    add tests for missing_ending_semicolon? method

---
+++ spec/error_spec.rb
@@ -0,0 +1,25 @@
+require_relative '../lib/errors.rb'
+
+RSpec.describe Errors do
+  include Errors
+
+  let(:data_index) { 1 }
+  let(:correct_sample) { '  font-size: 2rem;' }
+
+  describe '#missing_ending_semicolon?' do
+    let(:file_data) { '  font-size: 2rem' }
+    context 'has a semicolon at the end of a declaration' do
+      it 'returns true' do
+        expect(missing_ending_semicolon?(correct_sample, data_index)).to be true
+      end
+    end
+    context 'missing a semicolon at the end of a declaration' do
+      it 'does not return true and displays the error' do
+        expect(missing_ending_semicolon?(file_data, data_index)).not_to be true
+      end
+      it 'returns nil' do
+        expect(missing_ending_semicolon?(file_data, data_index)).to be nil
+      end
+    end
+  end
+end
