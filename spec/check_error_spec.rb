e7a8dcc2592c0af9fe7329438f231aa2544d12c1
Mupa1 <mupasmail@gmail.com>
2020-06-08 19:40:10 +0200

    add tests for the initialize method

--- spec/check_error_spec.rb
+++ spec/check_error_spec.rb
@@ -1,5 +1,21 @@
 require_relative '../lib/check_error.rb'
 
 RSpec.describe ErrorChecker do
-  
+  let(:user_file) { File.open('stylesheet.css') }
+  let(:file_data) { user_file.readlines }
+  let(:error_checker) { ErrorChecker.new(file_data) }
+
+  describe "#initialize" do
+    context 'given required argument' do
+      it "creates the ErrorChecker class instance" do
+        expect(ErrorChecker.class).to eql(Class)
+        expect(error_checker.file_data_arr).to eql(file_data)
+      end
+    end
+    context 'missing argument' do
+      it "does not create the ErrorChecker class instance" do
+        expect { ErrorChecker.new }.to raise_error(ArgumentError)
+      end
+    end   
+  end
 end
