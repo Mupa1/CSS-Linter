f16635e88017c623593c2c6f78a7123e5b74ffc4
Mupa1 <mupasmail@gmail.com>
2020-06-09 22:29:58 +0200

    refactor spec files

--- spec/check_error_spec.rb
+++ spec/check_error_spec.rb
@@ -1,37 +1,23 @@
 require_relative '../lib/check_error.rb'
 
 RSpec.describe ErrorChecker do
-  let(:user_file) { File.open('stylesheet.css') }
-  let(:file_data) { user_file.readlines }
-  let(:error_checker) { ErrorChecker.new(file_data) }
+  let(:user_file2) { File.open('stylesheet_copy.css') }
+  let(:file_data2) { user_file2.readlines }
+  let(:error_checker2) { ErrorChecker.new(file_data2) }
 
   describe '#initialize' do
-    context 'given required argument' do
-      it 'creates an instance of the ErrorChecker class' do
-        expect(ErrorChecker.class).to eql(Class)
-        expect(error_checker.file_data_arr).to eql(file_data)
-      end
+    it 'creates an instance of the ErrorChecker class' do
+      expect(ErrorChecker.class).to eql(Class)
+      expect(error_checker2.file_data_arr).to eql(file_data2)
     end
-    context 'missing argument' do
-      it 'does not create an instance of the ErrorChecker class' do
-        expect { ErrorChecker.new }.to raise_error(ArgumentError)
-      end
+    it 'raises an argument error if argument is missing' do
+      expect { ErrorChecker.new }.to raise_error(ArgumentError)
     end
   end
 
   describe '#display_error' do
-    context 'when the given file has errors' do
-      it 'it shows error messages and does not return true' do
-        expect(error_checker.display_error).not_to eql(true)
-      end
-    end
-    context 'when the given file does not have errors' do
-      let(:user_file2) { File.open('stylesheet_copy.css') }
-      let(:file_data2) { user_file2.readlines }
-      let(:error_checker2) { ErrorChecker.new(file_data2) }
-      it "returns nil and displays a message 'No Errors Found'" do
-        expect(error_checker2.display_error).to eql(nil)
-      end
+    it "displays a message 'No Errors Found' if user's file has no errors" do
+      expect { error_checker2.display_error }.to output(String).to_stdout
     end
   end
 end
--- spec/error_spec.rb
+++ spec/error_spec.rb
@@ -8,103 +8,61 @@ RSpec.describe Errors do
 
   describe '#missing_ending_semicolon?' do
     let(:file_data) { '  font-size: 2rem' }
-    context 'has a semicolon at the end of a declaration' do
-      it 'returns true' do
-        expect(missing_ending_semicolon?(correct_sample, data_index)).to be true
-      end
+    it 'returns true if there is a semicolon at the end of a declaration' do
+      expect(missing_ending_semicolon?(correct_sample, data_index)).to be true
     end
-    context 'missing a semicolon at the end of a declaration' do
-      it 'does not return true' do
-        expect(missing_ending_semicolon?(file_data, data_index)).not_to be true
-      end
-      it 'returns nil and it displays an error message' do
-        expect(missing_ending_semicolon?(file_data, data_index)).to be nil
-      end
+    it 'prints error message if semicolon is missing' do
+      expect { missing_ending_semicolon?(file_data, data_index) }.to output(String).to_stdout
     end
   end
 
   describe '#space_before_semicolon?' do
     let(:file_data) { '  font-size: 2rem ;' }
-    context 'has no space before the semicolon at the end of a declaration' do
-      it 'returns true' do
-        expect(space_before_semicolon?(correct_sample, data_index)).to be true
-      end
+    it 'returns true if there is no space before the semicolon' do
+      expect(space_before_semicolon?(correct_sample, data_index)).to be true
     end
-    context 'has a space before the semicolon at the end of a declaration' do
-      it 'does not return true' do
-        expect(space_before_semicolon?(file_data, data_index)).not_to be true
-      end
-      it 'returns nil and displays an error message' do
-        expect(space_before_semicolon?(file_data, data_index)).to be nil
-      end
+    it 'prints error message if there is a space before the semicolon' do
+      expect { space_before_semicolon?(file_data, data_index) }.to output(String).to_stdout
     end
   end
 
   describe '#indentation_error?' do
     let(:file_data) { 'font-size: 2rem;' }
-    context 'has two spaced indentation before the declaration' do
-      it 'returns true' do
-        expect(indentation_error?(correct_sample, data_index)).to be true
-      end
+    it 'returns true if it has two spaced indentation before the declaration' do
+      expect(indentation_error?(correct_sample, data_index)).to be true
     end
-    context 'has no indentation before the declaration' do
-      it 'does not return true' do
-        expect(indentation_error?(file_data, data_index)).not_to be true
-      end
-      it 'returns nil and displays an error message' do
-        expect(indentation_error?(file_data, data_index)).to be nil
-      end
+    it 'prints error message if no indentation before the declaration' do
+      expect { indentation_error?(file_data, data_index) }.to output(String).to_stdout
     end
   end
 
   describe '#space_after_colon?' do
     let(:file_data) { '  font-size:2rem;' }
-    context 'has a single space after the colon on the declaration' do
-      it 'returns true' do
-        expect(space_after_colon?(correct_sample, data_index)).to be true
-      end
+    it 'returns true if there is a single space after the colon' do
+      expect(space_after_colon?(correct_sample, data_index)).to be true
     end
-    context 'has no space after the colon on the declaration' do
-      it 'does not return true' do
-        expect(space_after_colon?(file_data, data_index)).not_to be true
-      end
-      it 'returns nil and displays an error message' do
-        expect(space_after_colon?(file_data, data_index)).to be nil
-      end
+    it 'prints error message if there is no space after the colon' do
+      expect { space_after_colon?(file_data, data_index) }.to output(String).to_stdout
     end
   end
 
   describe '#space_before_colon?' do
     let(:file_data) { '  font-size : 2rem;' }
-    context 'has no space before the colon on the declaration' do
-      it 'returns true' do
-        expect(space_before_colon?(correct_sample, data_index)).to be true
-      end
+    it 'returns true if there is no space before the colon on the declaration' do
+      expect(space_before_colon?(correct_sample, data_index)).to be true
     end
-    context 'has a space before the colon on the declaration' do
-      it 'does not return true' do
-        expect(space_before_colon?(file_data, data_index)).not_to be true
-      end
-      it 'returns nil and displays an error message' do
-        expect(space_before_colon?(file_data, data_index)).to be nil
-      end
+    it 'prints error message if there is a space before the colon' do
+      expect { space_before_colon?(file_data, data_index) }.to output(String).to_stdout
     end
   end
 
   describe '#trailing_white_space?' do
     let(:file_data) { '  font-size: 2rem;    ' }
-    context 'has no trailing white spaces after the declaration' do
-      it 'returns true' do
-        expect(trailing_white_space?(correct_sample, data_index)).to be true
-      end
+    it 'returns true if there is no trailing white spaces after the declaration' do
+      expect(trailing_white_space?(correct_sample, data_index)).to be true
     end
-    context 'has trailing white spaces after the declaration' do
-      it 'does not return true' do
-        expect(trailing_white_space?(file_data, data_index)).not_to be true
-      end
-      it 'returns nil and displays an error message' do
-        expect(trailing_white_space?(file_data, data_index)).to be nil
-      end
+    it 'prints error message if there is trailing white spaces after the declaration' do
+      expect { trailing_white_space?(file_data, data_index) }.to output(String).to_stdout
     end
   end
 end
